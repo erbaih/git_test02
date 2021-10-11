3736de88f4707256fdd5f387eba73e4b54eb0959
tom_glb <goodMorning@atguigu.com>
2021-10-11 22:03:18 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello master");
         System.out.println("hello hot_fix");
+        System.out.println("push test");
 
     }
 }

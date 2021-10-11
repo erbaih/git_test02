a720c5ea9c3cc8951461091c19b92bd1b5d6a16f
erbaih <88603273+erbaih@users.noreply.github.com>
2021-10-11 22:07:15 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hot_fix");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 }
